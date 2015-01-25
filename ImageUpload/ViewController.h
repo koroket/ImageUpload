2b8ac509ea5f7085d48b98a9f95564ad38513c4a
koroket <bmasakazu@gmail.com>
2015-01-25 03:45:14 -0800

    Initial Commit

---
+++ ImageUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageUpload
+//
+//  Created by sloot on 1/25/15.
+//  Copyright (c) 2015 sloot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageUploadTests/ImageUploadTests.m
@@ -0,0 +1,40 @@
+//
+//  ImageUploadTests.m
+//  ImageUploadTests
+//
+//  Created by sloot on 1/25/15.
+//  Copyright (c) 2015 sloot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ImageUploadTests : XCTestCase
+
+@end
+
+@implementation ImageUploadTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
