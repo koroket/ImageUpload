67b10614747d5c0b2ef4fee2b053e185a432e24a
koroket <bmasakazu@gmail.com>
2015-01-25 08:10:25 -0800

    upload

--- ImageUpload/ViewController.h
+++ ImageUpload/ViewController.h
@@ -7,8 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import <Foundation/Foundation.h>
+@interface ViewController : UIViewController<UIImagePickerControllerDelegate>
 
 
 @end
--- ImageUpload/ViewController.m
+++ ImageUpload/ViewController.m
@@ -1,27 +1,60 @@
+
 //
 //  ViewController.m
 //  ImageUpload
 //
-//  Created by sloot on 1/25/15.
-//  Copyright (c) 2015 sloot. All rights reserved.
+//  Created by Dhaivat Pandya on 1/25/15.
+//  Copyright (c) 2015 Dhaivat Pandya. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (retain, nonatomic) IBOutlet UIImageView *picture;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonPressed:(id)sender {
+    UIImagePickerController* imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    } else {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePicker animated:YES completion:^{
+    }];
+}
+
+- (void) imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo{
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+    self.picture.image = image;
+    NSString* fixedUrl = @"172.31.97.181/put_image";
+    NSURL *url = [NSURL URLWithString:fixedUrl];
+    //  NSURlcon
+    
 }
 
 @end
